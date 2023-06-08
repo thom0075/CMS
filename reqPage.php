3b10af6c464a87174f91811ee1ea903ed208c3be
thoma <thomascellitti@aol.com>
2023-06-08 15:07:56 +0200

    Connected reqpage to database

--- reqPage.php
+++ reqPage.php
@@ -4,6 +4,22 @@ if($title = filter_input(INPUT_GET,"one")){
     echo $title;
 }
 */
-echo $_GET['one'];
-echo $_GET['web'];
+if($_GET['page']){
+    $title = $_GET['page'];
+    echo $_GET['page'];
+}
+
+//echo $_GET['web'];
+
+$con = new PDO('mysql:host=localhost; dbname=abc', 'root', '');
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$stmt_pageContent = $con->prepare('SELECT title, content FROM cmsblock where title = ?');
+$stmt_pageContent->execute(array($title));
+
+$result = $stmt_pageContent->fetchAll();
+$content = $result[0]["content"];
+echo $content;
+
+
+//var_dump($result);
 ?>
